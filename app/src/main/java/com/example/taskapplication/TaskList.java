01c90be90b794da2158c824f56e418dbc10f5e73
Zs01t <bekeffy1@gmail.com>
2024-06-20 19:51:13 +0200

    Added tasklist, not sure if I have time to implement it

---
+++ app/src/main/java/com/example/taskapplication/TaskList.java
@@ -0,0 +1,16 @@
+//package com.example.taskapplication;
+//
+//
+//import androidx.room.ColumnInfo;
+//import androidx.room.Entity;
+//import androidx.room.PrimaryKey;
+//
+//@Entity
+//public class TaskList {
+//
+//    @PrimaryKey(autoGenerate = true)
+//    private int mId;
+//
+//    @ColumnInfo(name ="list_name")
+//    private String mListName;
+//}
